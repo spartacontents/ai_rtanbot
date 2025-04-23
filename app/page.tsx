450e9268dd77c7a3d7ee68da74dfa4c35b6f356c
spartacontents <152616195+spartacontents@users.noreply.github.com>
2025-04-23 15:23:40 +0900

    page1 배경이미지 비율 수정

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,19 @@ import Link from "next/link"
 export default function Home() {
   return (
     <main className="min-h-screen relative overflow-hidden" style={{ backgroundColor: '#ffffff' }}>
-      <div className="absolute inset-0">
+      <div className="absolute inset-0 flex items-center justify-center">
         <Image
           src="/page1.png"
           alt="메인 이미지"
-          fill
+          width={402}
+          height={402}
           className="object-contain"
           priority
         />
       </div>
 
       <div className="relative z-10 min-h-screen flex flex-col items-center justify-center">
-        <div className="absolute left-1/2 bottom-[17%] transform -translate-x-1/2 w-[80%] max-w-[300px]">
+        <div className="absolute left-1/2 bottom-[10%] transform -translate-x-1/2 w-[80%] max-w-[300px]">
           <Link href="/worry">
             <Image
               src="/page1 btn.png"
